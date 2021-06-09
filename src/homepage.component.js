93da41f7d190a56df74ddc46db55222b24d6d9ea
Andre Richardson <andrerichards@Andres-MacBook-Pro.local>
2021-06-09 06:35:23 -0400

    update dir text style

--- src/homepage.component.js
+++ src/homepage.component.js
@@ -7,31 +7,31 @@ const HomePage = () => (
             <div className="menu-item">
                 <div className="content">
                     <h1 className="title">Hats</h1>
-                    <span className="subtitle">SHOP NOW</span>
+                    <span className="subtitle">Shop Now</span>
                 </div>
             </div>
             <div className="menu-item">
                 <div className="content">
                     <h1 className="title">Jackets</h1>
-                    <span className="subtitle">SHOP NOW</span>
+                    <span className="subtitle">Shop Now</span>
                 </div>
             </div>
             <div className="menu-item">
                 <div className="content">
                     <h1 className="title">Hats</h1>
-                    <span className="subtitle">SHOP NOW</span>
+                    <span className="subtitle">Shop Now</span>
                 </div>
             </div>
             <div className="menu-item">
                 <div className="content">
                     <h1 className="title">Womens</h1>
-                    <span className="subtitle">SHOP NOW</span>
+                    <span className="subtitle">Shop Now</span>
                 </div>
             </div>
             <div className="menu-item">
                 <div className="content">
                     <h1 className="title">Mens</h1>
-                    <span className="subtitle">SHOP NOW</span>
+                    <span className="subtitle">Shop Now</span>
                 </div>
             </div>
         </div>
